55749bcea923ae8157db93a4fb91450eff8d6935
stefan4012it <stefanra555@gmail.com>
2021-03-05 20:15:27 +0100

    ultimate bid

--- src/Header/promotionBox2/promotionBox2.tsx
+++ src/Header/promotionBox2/promotionBox2.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Radium from 'radium';
 import './promotionBox2.css';
+import img1 from '../Data/img/_DSC0729.jpg';
 
 const promoBox2Representation = () => {
     return (
-        <div className="promotion-box-2"></div>
+        <div className="promotion-box-2">
+        </div>
     )
 }
 
